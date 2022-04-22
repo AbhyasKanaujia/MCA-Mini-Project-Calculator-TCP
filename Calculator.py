1adae75e701d2d8bc16bc3f2ca421cfa9a1e4f63
AbhyasKanaujia <44172451+AbhyasKanaujia@users.noreply.github.com>
2022-04-22 22:35:26 +0530

    Increased Font size, monospace font style

--- Calculator.py
+++ Calculator.py
@@ -1,26 +1,29 @@
 from tkinter import *
+import tkinter.font as tkFont
 import socket
 
 root = Tk()
-root.title("Simple Calculator")
+root.title('Simple Calculator')
+tkFont.nametofont('TkDefaultFont').configure(size=12)
+
 
 # Create Display
-e = Entry(root, width=60, borderwidth=4,
-          state='disabled', disabledbackground='white', disabledforeground='black')
+e = Entry(root, width=30, borderwidth=4,
+          state='disabled', disabledbackground='white', disabledforeground='black', font='Courier 15 bold')
 # Create Quick Result Label
-ResultLabel = Label(root, text="", justify='left', anchor='w')
+ResultLabel = Label(root, text='', justify='left', anchor='w', font='Courier')
 
 
 def getResultFromServer(expression):
     host = socket.gethostname()
-    print("Sending Request to Server: ", expression)
+    print('Sending Request to Server: ', expression)
     port = 5000
     client = socket.socket()
     client.connect((host, port))
     client.send(expression.encode())
 
     result = client.recv(1024).decode()
-    print("Received From Server: ", result)
+    print('Received From Server: ', result)
 
     client.close()
 
@@ -33,10 +36,10 @@ operator = False
 
 def equal_click():
     e.config(state='normal')
-    if e.get() != "":
+    if e.get() != '':
         ans = getResultFromServer(e.get())
 
-    ResultLabel.config(text="")
+    ResultLabel.config(text='')
     e.delete(0, END)
     e.insert(0, ans)
     e.config(state='disabled')
@@ -61,7 +64,7 @@ def clear_click():
     e.delete(0, END)
     e.config(state='disable')
 
-    ResultLabel.config(text="")
+    ResultLabel.config(text='')
     global operator
     operator = False
 
@@ -91,46 +94,46 @@ def operator_click(symbol):
 
 
 # Create Number Buttons
-button_1 = Button(root, text="1", padx=40, pady=20,
+button_1 = Button(root, text='1', padx=40, pady=20,
                   command=lambda: digit_click(1))
-button_2 = Button(root, text="2", padx=40, pady=20,
+button_2 = Button(root, text='2', padx=40, pady=20,
                   command=lambda: digit_click(2))
-button_3 = Button(root, text="3", padx=40, pady=20,
+button_3 = Button(root, text='3', padx=40, pady=20,
                   command=lambda: digit_click(3))
-button_4 = Button(root, text="4", padx=40, pady=20,
+button_4 = Button(root, text='4', padx=40, pady=20,
                   command=lambda: digit_click(4))
-button_5 = Button(root, text="5", padx=40, pady=20,
+button_5 = Button(root, text='5', padx=40, pady=20,
                   command=lambda: digit_click(5))
-button_6 = Button(root, text="6", padx=40, pady=20,
+button_6 = Button(root, text='6', padx=40, pady=20,
                   command=lambda: digit_click(6))
-button_7 = Button(root, text="7", padx=40, pady=20,
+button_7 = Button(root, text='7', padx=40, pady=20,
                   command=lambda: digit_click(7))
-button_8 = Button(root, text="8", padx=40, pady=20,
+button_8 = Button(root, text='8', padx=40, pady=20,
                   command=lambda: digit_click(8))
-button_9 = Button(root, text="9", padx=40, pady=20,
+button_9 = Button(root, text='9', padx=40, pady=20,
                   command=lambda: digit_click(9))
-button_0 = Button(root, text="0", padx=40, pady=20,
+button_0 = Button(root, text='0', padx=40, pady=20,
                   command=lambda: digit_click(0))
 
-button_decimal = Button(root, text=".", padx=40, pady=20,
+button_decimal = Button(root, text='.', padx=43, pady=20,
                         command=lambda: operator_click('.'))
 # Create Operation Buttons
-button_add = Button(root, text="+", padx=38, pady=20,
+button_add = Button(root, text='+', padx=38, pady=20,
                     command=lambda: operator_click('+'), bg='#E1D5E9')
-button_multiply = Button(root, text="*", padx=40, pady=20,
+button_multiply = Button(root, text='*', padx=40, pady=20,
                          command=lambda: operator_click('*'), bg='#E1D5E9')
-button_divide = Button(root, text="/", padx=40, pady=20,
+button_divide = Button(root, text='/', padx=40, pady=20,
                        command=lambda: operator_click('/'), bg='#E1D5E9')
-button_subtract = Button(root, text="-", padx=40, pady=20,
+button_subtract = Button(root, text='-', padx=40, pady=20,
                          command=lambda: operator_click('-'), bg='#E1D5E9')
-button_modulus = Button(root, text="%", padx=40, pady=20,
+button_modulus = Button(root, text='%', padx=38, pady=20,
                         command=lambda: operator_click('%'), bg='#E1D5E9')
 # Create Function Buttons
-button_equal = Button(root, text="=", padx=185, pady=20,
+button_equal = Button(root, text='=', padx=189, pady=20,
                       command=equal_click, bg='#8C28DA')
-button_clear = Button(root, text="Clear", padx=77,
+button_clear = Button(root, text='Clear', padx=77,
                       pady=20, command=clear_click, bg='#FEB92D')
-button_backspace = Button(root, text="<", padx=89, pady=20,
+button_backspace = Button(root, text='<', padx=89, pady=20,
                           command=backspace_click, bg='#FFE3A3')
 
 # place widgets on screen
